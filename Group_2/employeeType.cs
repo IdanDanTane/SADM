e743f878af80934947108b995675b5ec8f0d6d00
עידן דן טנא <idandan@post.bgu.ac.il>
2023-06-04 17:32:47 +0300

    add to main project

--- Group_2/employeeType.cs
+++ Group_2/employeeType.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,10 @@ namespace Group_2
 {
     public enum employeeType
     {
-        CEO,
-        Shift_manager,
-        Food_technologist,
-        Stock_keeper,
-        Ware_house_manager
+        [Description("CEO")] CEO,
+        [Description("Shift manager")] Shift_manager,
+        [Description("Food technologist")] Food_technologist,
+        [Description("Stock keeper")]  Stock_keeper,
+        [Description("Warehouse manager")] Ware_house_manager
     }
 }
