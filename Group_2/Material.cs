63429e5185c3ed67e3d48bdf5a3e68f4b00d2629
עידן דן טנא <idandan@post.bgu.ac.il>
2023-06-04 19:24:18 +0300

    add classes

---
+++ Group_2/Material.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Protocols.WSTrust;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group_2
+{
+    internal class Material
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+
+        public decimal pricePerTon { get; set; }
+
+        public decimal minimumThreshold { get; set; }
+
+        public Mstatus Status { get; set; }
+
+        public Warehouse Location { get; set; }
+
+        public DateTime recivedDate { get; set; }
+
+        public DateTime expirationDate  { get; set; }
+
+        public decimal amount { get; set; }
+
+        public Material(string id,string name, decimal price,decimal threshold, Mstatus status, Warehouse loc, DateTime recived,DateTime exp, decimal amount, bool isNew)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.pricePerTon = price;
+            this.minimumThreshold = threshold;
+            this.Status = status;
+            this.Location = loc;
+            this.recivedDate = recived;
+            this.expirationDate = exp;
+            this.amount = amount;
+            if(isNew)
+            {
+                Program.Materials.Add(this);
+            }
+        }
+    }
+}
