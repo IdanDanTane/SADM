bf38a44e9fb85fb4e5be37e6a40c07c9ccc76bd6
Nitayyeffet <Nitayy@post.bgu.ac.il>
2023-06-05 19:36:27 +0300

    lll

---
+++ Group_2/createProduct.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace Group_2
+{
+    partial class createProduct
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.productID = new System.Windows.Forms.TextBox();
+            this.productName = new System.Windows.Forms.TextBox();
+            this.pricePerTon = new System.Windows.Forms.TextBox();
+            this.expirationDate = new System.Windows.Forms.DateTimePicker();
+            this.ProductType = new System.Windows.Forms.Label();
+            this.Type = new System.Windows.Forms.ComboBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.getMaterials = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.Amount = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(120, 145);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(120, 186);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 16);
+            this.label2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(110, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Product ID : ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(110, 220);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Product name :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(110, 273);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(102, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Expiration date :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(110, 327);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(88, 16);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Price per ton :";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(341, 382);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(156, 36);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Create product";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // productID
+            // 
+            this.productID.Location = new System.Drawing.Point(236, 158);
+            this.productID.Name = "productID";
+            this.productID.Size = new System.Drawing.Size(125, 22);
+            this.productID.TabIndex = 7;
+            // 
+            // productName
+            // 
+            this.productName.Location = new System.Drawing.Point(236, 214);
+            this.productName.Name = "productName";
+            this.productName.Size = new System.Drawing.Size(125, 22);
+            this.productName.TabIndex = 8;
+            // 
+            // pricePerTon
+            // 
+            this.pricePerTon.Location = new System.Drawing.Point(236, 324);
+            this.pricePerTon.Name = "pricePerTon";
+            this.pricePerTon.Size = new System.Drawing.Size(125, 22);
+            this.pricePerTon.TabIndex = 10;
+            // 
+            // expirationDate
+            // 
+            this.expirationDate.CustomFormat = "yyyy-MM-dd";
+            this.expirationDate.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.expirationDate.Location = new System.Drawing.Point(236, 267);
+            this.expirationDate.Name = "expirationDate";
+            this.expirationDate.Size = new System.Drawing.Size(125, 22);
+            this.expirationDate.TabIndex = 11;
+            // 
+            // ProductType
+            // 
+            this.ProductType.AutoSize = true;
+            this.ProductType.Location = new System.Drawing.Point(110, 102);
+            this.ProductType.Name = "ProductType";
+            this.ProductType.Size = new System.Drawing.Size(88, 16);
+            this.ProductType.TabIndex = 12;
+            this.ProductType.Text = "Product type :";
+            // 
+            // Type
+            // 
+            this.Type.FormattingEnabled = true;
+            this.Type.Location = new System.Drawing.Point(236, 102);
+            this.Type.Name = "Type";
+            this.Type.Size = new System.Drawing.Size(125, 24);
+            this.Type.TabIndex = 13;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(458, 102);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(71, 16);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Materials  :";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.getMaterials,
+            this.Amount});
+            this.dataGridView1.Location = new System.Drawing.Point(461, 135);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(291, 67);
+            this.dataGridView1.TabIndex = 16;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // getMaterials
+            // 
+            this.getMaterials.HeaderText = "Material";
+            this.getMaterials.MinimumWidth = 6;
+            this.getMaterials.Name = "getMaterials";
+            this.getMaterials.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.getMaterials.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.getMaterials.Width = 125;
+            // 
+            // Amount
+            // 
+            this.Amount.HeaderText = "Amount";
+            this.Amount.MinimumWidth = 6;
+            this.Amount.Name = "Amount";
+            this.Amount.Width = 125;
+            // 
+            // createProduct
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.Type);
+            this.Controls.Add(this.ProductType);
+            this.Controls.Add(this.expirationDate);
+            this.Controls.Add(this.pricePerTon);
+            this.Controls.Add(this.productName);
+            this.Controls.Add(this.productID);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "createProduct";
+            this.Text = "Create product";
+            this.Load += new System.EventHandler(this.createProduct_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox productID;
+        private System.Windows.Forms.TextBox productName;
+        private System.Windows.Forms.TextBox pricePerTon;
+        private System.Windows.Forms.DateTimePicker expirationDate;
+        private System.Windows.Forms.Label ProductType;
+        private System.Windows.Forms.ComboBox Type;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewComboBoxColumn getMaterials;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Amount;
+    }
+}
---
+++ Group_2/createProduct.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Group_2
+{
+    public partial class createProduct : Form
+    {
+        public createProduct()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void createProduct_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            String ProductMaterials = dataGridView1
+        
+    }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
